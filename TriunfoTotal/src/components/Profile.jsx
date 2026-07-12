5e44ef2e318434ed78c31447063ade1bb9b26109
Micaela Chans <134005084+MicaelaChans@users.noreply.github.com>
2026-07-12 19:45:06 -0300

    .

--- TriunfoTotal/src/components/Profile.jsx
+++ TriunfoTotal/src/components/Profile.jsx
@@ -321,6 +321,20 @@ function Profile() {
                 {saving ? "Guardando..." : "Guardar cambios"}
               </button>
 
+              <section className="container mt-3">
+                <div>
+                  <h2>Historial de Torneos</h2>
+                  <div class="card" style="width: 18rem;">
+                    <img src="" alt="" /> src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                  </div>
+                </div>
+              </section>
+
             </div>
 
           </form>
